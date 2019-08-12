16b27ce5ff3af15c2e512ec838071656451a95b9
forderation <kharisma.muzaki@gmail.com>
2019-08-12 17:50:06 +0700

    fix grid layout

--- app/src/main/java/com/example/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/example/moviecatalogue/MainActivity.java
@@ -358,7 +358,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         Intent intent;
